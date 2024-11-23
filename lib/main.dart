f0ce442a3faa2f74610bff4af35397bf2812cc6a
adesh <>
2024-11-23 15:04:50 +0530

    Flutter flame

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,11 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'package:geometry_dash/services/settings_service.dart';
 
 import 'services/app.locator.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupLocator();
-  runApp(MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  MainApp({super.key});
-
-  final SettingsService _settingsService = locator<SettingsService>();
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Hello World!',
-            style: TextStyle(
-                color: _settingsService.isDarkMode ? Colors.black : Colors.red),
-          ),
-        ),
-      ),
-    );
-  }
+  final game = FlameGame();
+  runApp(GameWidget(game: game));
 }
