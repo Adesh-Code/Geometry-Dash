8f8faa01442c82fb091ede4e7113f4d1d54d04e6
adesh <141008372+Adesh-Code@users.noreply.github.com>
2024-11-23 15:10:01 +0530

    Requirements (#1)
    * added service logic
    
    * Flutter flame
    
    * Flame Audio
    
    * Plugins
    
    ---------
    
    Co-authored-by: adesh <>

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+import 'services/app.locator.dart';
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupLocator();
+  final game = FlameGame();
+  runApp(GameWidget(game: game));
 }
